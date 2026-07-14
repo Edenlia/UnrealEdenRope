f8236df2b67b0dc251ce486257785f1be9b57aa8
Edenlia <zsy010906@gmail.com>
2026-07-14 15:13:10 +0800

    First Person template

---
+++ Source/UnrealEdenRope/UnrealEdenRope.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealEdenRope.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealEdenRope, "UnrealEdenRope" );
+
+DEFINE_LOG_CATEGORY(LogUnrealEdenRope)
